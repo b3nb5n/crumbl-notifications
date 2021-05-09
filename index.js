34f8b61ad432b034977a9cbc19f616ddbd77cf7f
Ben Baldwin <ben@baldwindesign.co>
2021-05-09 11:31:05 -0700

    configured to work with cloud run

--- index.js
+++ index.js
@@ -3,38 +3,48 @@ const twilio = require('twilio');
 const app = require('express')();
 require('dotenv').config();
 
-app.post('/', async (_, res) => {
-	const browser = await puppeteer.launch({
-		args: ['--no-sandbox', '--disable-gpu'],
-	});
-	const page = await browser.newPage();
+app.post('/', async (req, res) => {
+	try {
+		const browser = await puppeteer.launch({
+			args: ['--no-sandbox', '--disable-gpu'],
+		});
+		const page = await browser.newPage();
 
-	await page.goto('https://crumblcookies.com/');
+		await page.goto('https://crumblcookies.com/');
 
-	const cookies = await page.evaluate(() =>
-		Array.from(document.querySelectorAll('#weekly-cookie-flavors h3')).map((element) =>
-			element.innerText.trim()
-		)
-	);
+		const cookies = await page.evaluate(() =>
+			Array.from(document.querySelectorAll('#weekly-cookie-flavors h3')).map((element) =>
+				element.innerText.trim()
+			)
+		);
 
-	await page.close();
-	await browser.close();
+		await page.close();
+		await browser.close();
 
-	const cookieList = cookies.map((name) => `🍪 ${name}`).join('\n');
-	const message = `This weeks Crumbl flavors are:\n${cookieList}\n\n`;
+		const cookieList = cookies.map((name) => `🍪 ${name}`).join('\n');
+		const message = `This weeks Crumbl flavors are:\n${cookieList}\n\n`;
 
-	const { TWILIO_SID, TWILIO_TOKEN, RECIPIANT_PHONE } = process.env;
-	const client = twilio(TWILIO_SID, TWILIO_TOKEN);
+		const recipiant = req.query.recipiant;
+		if (!recipiant) return res.send(message);
 
-	res.send(message);
-	await client.messages.create({
-		body: message,
-		to: RECIPIANT_PHONE,
-		from: '+14804000695',
-	});
+		const { TWILIO_SID, TWILIO_TOKEN } = process.env;
+		const client = twilio(TWILIO_SID, TWILIO_TOKEN);
+
+		await client.messages.create({
+			body: message,
+			to: recipiant,
+			from: '+14804000695',
+		});
+
+		return res.send(message);
+	} catch (err) {
+		console.error(err);
+		res.status(500);
+		res.end();
+	}
 });
 
-const port = 80;
+const port = 8080;
 app.listen(port, () => {
 	console.log(`listening on port ${port}...`);
 });
