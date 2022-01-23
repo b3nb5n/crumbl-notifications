86f9d59d8da703eb79a9c35b0a098cbabafdd044
Ben Baldwin <benbaldwin000@gmail.com>
2022-01-23 12:55:01 -0700

    fixed it ...hopefully

--- index.ts
+++ index.ts
@@ -11,9 +11,8 @@ interface Flavors {
 const firebase = admin.initializeApp();
 const CRUMBL_URL = 'crumblcookies.com';
 const runtimeOptions: functions.RuntimeOptions = {
-	memory: '256MB',
-	timeoutSeconds: 30,
-	maxInstances: 1,
+	memory: '512MB',
+	timeoutSeconds: 60,
 };
 
 const getRecipients = async () => {
@@ -26,11 +25,9 @@ const getFlavors = async (): Promise<Flavors> => {
 	const browser = await puppeteer.launch();
 	const page = (await browser.pages())[0] ?? (await browser.newPage());
 
-	await page.goto(CRUMBL_URL);
+	await page.goto(`https://${CRUMBL_URL}`);
 	const flavors = (await page.evaluate(() =>
-		Array.from(
-			document.querySelectorAll('#weekly-cookie-flavors h3:nth-child(-n+6)')
-		)
+		Array.from(document.querySelectorAll('#weekly-cookie-flavors h3:nth-child(-n+6)'))
 			.map((element) => element.textContent?.trim())
 			.filter((flavor) => typeof flavor === 'string')
 	)) as string[];
@@ -46,9 +43,7 @@ const getFlavors = async (): Promise<Flavors> => {
 
 const formatFlavors = (flavors: Flavors) => {
 	const cookieFlavors = flavors.cookies.map((flavor) => `🍪 ${flavor}`).join('\n');
-	const iceCreamFlavors = flavors.iceCream
-		.map((flavor) => `🍦 ${flavor}`)
-		.join('\n');
+	const iceCreamFlavors = flavors.iceCream.map((flavor) => `🍦 ${flavor}`).join('\n');
 
 	const TITLE = 'This weeks crumbl flavors are:';
 	return `${TITLE}\n\n${cookieFlavors}\n\n${iceCreamFlavors}\n\n${CRUMBL_URL}`;
@@ -59,12 +54,7 @@ const getTwilioClient = () => {
 	return twilio(sid, token);
 };
 
-const notify = async (
-	smsClient: twilio.Twilio,
-	from: string,
-	to: string,
-	body: string
-) => {
+const notify = async (smsClient: twilio.Twilio, from: string, to: string, body: string) => {
 	try {
 		functions.logger.debug(`Notifying ${to}`);
 		await smsClient.messages.create({ from, to, body });
@@ -86,40 +76,35 @@ const crumblNotifier = async (recipients: string[]) => {
 	}
 };
 
-if (process.env.FUNCTIONS_EMULATOR) {
-	exports.getFlavors = functions
-		.runWith(runtimeOptions)
-		.https.onRequest(async (_, res) => {
-			try {
-				const flavors = await getFlavors();
-				res.send(flavors);
-			} catch (e) {
-				res.status(500).send({ error: e });
-			}
-		});
+exports.getFlavors = functions.runWith(runtimeOptions).https.onRequest(async (_, res) => {
+	try {
+		const flavors = await getFlavors();
+		res.send(flavors);
+	} catch (e) {
+		res.status(500).send({ error: e });
+	}
+});
 
-	exports.notify = functions
-		.runWith(runtimeOptions)
-		.https.onRequest(async (req, res) => {
-			try {
-				const recipients =
-					typeof req.query.recipient === 'string'
-						? [req.query.recipient as string]
-						: await getRecipients();
+exports.notify = functions.runWith(runtimeOptions).https.onRequest(async (req, res) => {
+	try {
+		const recipients =
+			typeof req.query.recipient === 'string'
+				? [req.query.recipient as string]
+				: await getRecipients();
 
-				await crumblNotifier(recipients);
-				res.sendStatus(200);
-			} catch (e) {
-				res.status(500).send(e);
-			}
-		});
-} else {
-	exports.notifier = functions
-		.runWith(runtimeOptions)
-		.pubsub.schedule('30 18 * * 0')
-		.timeZone('America/Phoenix')
-		.onRun(async () => {
-			const recipients = await getRecipients();
-			await crumblNotifier(recipients);
-		});
-}
+		await crumblNotifier(recipients);
+		res.sendStatus(200);
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
+exports.notifier = functions
+	.runWith(runtimeOptions)
+	.pubsub.schedule('30 18 * * 0')
+	.timeZone('America/Phoenix')
+	.onRun(async () => {
+		console.log('Running notifier');
+		const recipients = await getRecipients();
+		await crumblNotifier(recipients);
+	});
